35abbdce8c6e9b3fb8af589ef5c26c8f55e5b7fd
TUSHAR <tusharsen11@gmail.com>
2023-08-30 02:18:08 +0530

    modify the java class

--- src/main/java/in/ashokit/Welcome.java
+++ src/main/java/in/ashokit/Welcome.java
@@ -2,7 +2,7 @@ package in.ashokit;
 
 public class Welcome {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello to the world World!"); 
     }
 
 }
